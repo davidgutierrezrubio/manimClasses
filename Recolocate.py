f7016ea8174b921bb99264b7f5158fd203865015
David Gutiérrez <davidgutierrezrubio@gmail.com>
2019-12-08 19:53:36 +0100

    Add files via upload

--- Recolocate.py
+++ Recolocate.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python
+from manimlib.imports import *
 
-import sys
-
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, 'c:\\manim\\manim-3feb')
-from big_ol_pile_of_manim_imports import *
-
-
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, 'c:\\manim\\manim-3feb')
-from big_ol_pile_of_manim_imports import *
-
-from  screengrid import *
-
-if __name__ == "__main__":
-    os.chdir('c:\\manim\\manim-3feb')
-    module_name = 'myProjects\\'+ os.path.basename(__file__)
-    print(module_name)
-    command_A = "python -m manim "
-    command_B = module_name +" " +"pruebaRecolocate"+" -pl"
-    os.system(command_A + command_B)
 class Recolocate(Homotopy):
     #Clase de animación para recolocar una figura
     def __init__(self,mobject,fromA,fromB,toA,toB,scaling=True,rotate=True,**kwargs):
@@ -54,62 +35,17 @@ class Recolocate(Homotopy):
         fu=Recolocate.get_recolocate_udpate_function(fromA,fromB,toA,toB,scaling)
         mobject.apply_function(lambda p:fu(*p,1))
 
-class recolocateFromToSimple(Recolocate):
+class RecolocateFromToSimple(Recolocate):
     def __init__(self,mobject,fromA,toA,angle,**kwargs):
         fromB=fromA+RIGHT
         toB=toA+np.array([np.cos(angle),np.sin(angle),0])
         super().__init__(mobject,fromA,fromB,toA,toB,False,**kwargs)
 
 
-class recolocateShiftSimple(Recolocate):
+class RecolocateShiftSimple(Recolocate):
     def __init__(self,mobject,vec,angle,**kwargs):
         fromA=np.array([0,0,0])
         fromB=fromA+RIGHT
         toA=fromA+vec
         toB=toA+np.array([np.cos(angle),np.sin(angle),0])
         super().__init__(mobject,fromA,fromB,toA,toB,False,**kwargs)
-        #return Homotopy(Recolocate.get_recolocate_udpate_function(fromA,fromB,toA,toB,False),mobject)
-class pruebaRecolocate(Scene):
-    def construct(self):
-        A=0*UP
-        B=2*RIGHT
-        C=3*UP
-        D=3*RIGHT
-        E=D+RIGHT
-        cua=Square().scale(.5).move_to(A) #Center at A
-        cir=Circle().scale(.5).move_to(B) #Center at B
-        self.add(cua)
-        self.add(cir)
-        # self.add(Dot(A))
-        vg=VGroup()
-        vg.add(Dot(A),TextMobject("A"))
-        self.add(vg)
-        # self.add(Dot(B))
-        # self.add(Dot(C))
-        # self.add(Dot(D))
-        self.add(Dot(E))
-        tri=Polygon(A,B,C,fill_color=RED,fill_opacity=1)
-        anim=Recolocate(tri,A,C,D,E)
-        self.play(anim)
-        anim.applyRtoObj(cua)
-        anim.applyRtoObj(cir)
-
-
-        anim=recolocateFromToSimple(tri,E,A,-PI/3)
-        self.play(anim)
-
-
-
-        anim.applyRtoObj(cua)
-        anim.applyRtoObj(cir)
-        anim=recolocateShiftSimple(tri,UP,PI/3)
-        self.play(anim)
-        anim.applyRtoObj(cua)
-        anim.applyRtoObj(cir)
-
-
-
-
-        # anim=Recolocate(tri,A,B,puntos[3].get_center(),puntos[3].get_center())
-        # self.play(anim)
-        self.wait(3)
---
+++ demoRecolocate.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+from manimlib.imports import *
+from Recolocate import *
+class demoRecolocate(Scene):
+    def dotWithLabel(self,coords,label,colorPoint):
+        vg=VGroup()
+        punto=Dot().shift(coords)
+        vg.add(punto)
+        punto.set_color(colorPoint)
+        te=TextMobject(label)
+        te.next_to(punto,DOWN)
+        te.set_color(colorPoint)
+        vg.add(te)
+        return vg
+    def construct(self):
+        A=-2*RIGHT-UP
+        B=2*RIGHT-UP
+        C=2.2*RIGHT+UP
+        D=3*RIGHT-.5*UP
+        rec=Rectangle()
+        texto=TextMobject("Recolocate(mobject,","A",",","B",",","C",",","D",")").to_edge(UP)
+        texto[1].set_color(RED)
+        texto[3].set_color(RED)
+        texto[5].set_color(BLUE)
+        texto[7].set_color(BLUE)
+
+        self.play(FadeIn(texto))
+
+        vgA=self.dotWithLabel(A,"A",RED)
+        vgB=self.dotWithLabel(B,"B",RED)
+        vgC=self.dotWithLabel(C,"C",BLUE)
+        vgD=self.dotWithLabel(D,"D",BLUE)
+        anims=map(lambda r:FadeIn(r),[vgA,vgB,vgC,vgD])
+        self.play(*anims)
+        self.play(ShowCreation(rec))
+        anim=Recolocate(rec,A,B,C,D)
+        self.play(anim)
+        self.wait(3)
+
+        #Borro
+        self.play(FadeOut(rec),FadeOut(texto))
+        #Ahora demo con recolocate scaling=False
+        texto=TextMobject("Recolocate(mobject,","A",",","B",",","C",",","D",",scaling=False)").to_edge(UP)
+        texto[1].set_color(RED)
+        texto[3].set_color(RED)
+        texto[5].set_color(BLUE)
+        texto[7].set_color(BLUE)
+        self.play(FadeIn(texto))
+        rec=Rectangle()
+        self.play(ShowCreation(rec))
+        anim=Recolocate(rec,A,B,C,D,scaling=False)
+        self.play(anim)
+        self.wait(3)
+
+        #Borro
+        self.play(FadeOut(rec),FadeOut(texto))
+        #Ahora demo con recolocate rotate=False
+        texto=TextMobject("Recolocate(mobject,","A",",","B",",","C",",","D",",rotate=False)").to_edge(UP)
+        texto[1].set_color(RED)
+        texto[3].set_color(RED)
+        texto[5].set_color(BLUE)
+        texto[7].set_color(BLUE)
+        self.play(FadeIn(texto))
+        rec=Rectangle()
+        self.play(ShowCreation(rec))
+        anim=Recolocate(rec,A,B,C,D,rotate=False)
+        self.play(anim)
+        self.wait(3)
+
+        #Borro
+        self.play(FadeOut(rec),FadeOut(texto),FadeOut(vgB),FadeOut(vgD))
+        #Ahora demo con recolocatesimple
+        texto=TextMobject("RecolocateFromToSimple(mobject,",
+        "A",
+        ",",
+        "C",
+        ",PI/3)").to_edge(UP)
+        texto[1].set_color(RED)
+        texto[3].set_color(BLUE)
+        self.play(FadeIn(texto))
+        rec=Rectangle()
+        self.play(ShowCreation(rec))
+        anim=RecolocateFromToSimple(rec,A,C,PI/4)
+        self.play(anim)
+        self.wait(3)
